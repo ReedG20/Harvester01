914d1d4af6c0adab7e82d3fed8dfa1f49cfb234c
ReedG20 <56209714+ReedG20@users.noreply.github.com>
2021-10-06 22:26:29 -0700

    Almost done w/ new terrain script w/ support for multiple biomes
    Just need to work on instantiating the objects within a given biome.

--- Assets/Scripts/GenerateTerrain.cs
+++ Assets/Scripts/GenerateTerrain.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.Rendering;
 
 public class GenerateTerrain : MonoBehaviour
 {
@@ -31,8 +30,10 @@ public class GenerateTerrain : MonoBehaviour
     Vector3 defaultRotation = new Vector3(-90f, 0f, 0f);
 
     // Larger = more detail
+    /*
     float defaultNoiseScale = 0.111f;
     float biomeNoiseScale = 0.033f;
+    */
     float scale = 2;
 
     // ^2 is amount of tiles
@@ -67,6 +68,11 @@ public class GenerateTerrain : MonoBehaviour
 
     // --------------
 
+    public Biome[] biomes;
+    public float[] randomNums;
+
+    float biomeNoiseScale = 0.01f;
+
     void Start()
     {
         randomPosition01 = Random.Range(-1000, 1000);
@@ -80,7 +86,62 @@ public class GenerateTerrain : MonoBehaviour
         CreateTerrain();
     }
 
-    ///*
+    void CreateTerrain()
+    {
+        for (int i = 0; i < biomes.Length; i++)
+        {
+            randomNums[i] = Random.Range(-1000f, 1000f);
+        }
+
+        for (int x = 0; x < size; x++)
+        {
+            for (int y = 0; y < size; y++)
+            {
+                // Each tile
+
+                float value = 0f;
+                int valueIndex = 0;
+
+                for (int i = 0; i < biomes.Length; i++)
+                {
+                    float currentValue = Mathf.PerlinNoise((x + randomNums[i]) * biomeNoiseScale, y * biomeNoiseScale);
+
+                    if (value < currentValue)
+                    {
+                        value = currentValue;
+                        valueIndex = i;
+                    }
+                }
+
+                // We know the coords of the tile, as well as which biome it is
+
+                CreateBiomeTile(x, y, valueIndex);
+            }
+        }
+    }
+
+    void CreateBiomeTile(int x, int y, int biome)
+    {
+        for (int i = 0; i < biomes[biome].biomeElements.Length; i++)
+        {
+            // This is where I am
+        }
+    }
+
+    // Function instantiates an object given the prefab and rotation
+    void InstantiateObject(GameObject prefab, Vector3 rotation)
+    {
+        Instantiate(prefab, new Vector3((_i - (size / 2)) * scale, 0f, (_j - (size / 2)) * scale), Quaternion.Euler(rotation));
+    }
+
+    // Generates boolean based on the perlin noise and the location of the tile
+    bool GenerateBool(float randomPosition, float cutoff, float noiseScale)
+    {
+        //Higher cutoff -> less likely return true
+        return Mathf.PerlinNoise((_i + randomPosition) * noiseScale, (_j + randomPosition) * noiseScale) >= cutoff;
+    }
+
+    /*
     void CreateTerrain()
     {
         for (int i = 0; i < size; i++)
@@ -90,11 +151,14 @@ public class GenerateTerrain : MonoBehaviour
             {
                 _j = j;
                 GenerateTile(GetBiome(i, j));
+
             }
         }
     }
     //*/
 
+    // Newish functions
+    /*
     // Returns biome as a number between 1 and 5 based on the given tile
     int GetBiome(int i, int j)
     {
@@ -220,6 +284,7 @@ public class GenerateTerrain : MonoBehaviour
             Debug.LogError("Biome int not in range!");
         }
     }
+    */
 
     // Old create terrain function
     /*
@@ -310,27 +375,23 @@ public class GenerateTerrain : MonoBehaviour
         }
     }
     //*/
+}
 
-    // Function instantiates an object given the prefab and rotation
-    void InstantiateObject(GameObject prefab, Vector3 rotation)
-    {
-        Instantiate(prefab, new Vector3((_i - (size / 2)) * scale, 0f, (_j - (size / 2)) * scale), Quaternion.Euler(rotation));
-    }
-
-    // Generates boolean based on the perlin noise and the location of the tile
-    bool GenerateBool(float randomPosition, float cutoff, float noiseScale)
-    {
-        //Higher cutoff -> less likely return true
-        return Mathf.PerlinNoise((_i + randomPosition) * noiseScale, (_j + randomPosition) * noiseScale) >= cutoff;
-    }
+// Environment element class
+[System.Serializable]
+public class Biome
+{
+    public BiomeElement[] biomeElements;
 }
 
 // Environment element class
 [System.Serializable]
 public class BiomeElement
 {
+    // 
     public GameObject prefab;
 
+    // 
     public float noiseScale;
 
     public float noisePosition;
